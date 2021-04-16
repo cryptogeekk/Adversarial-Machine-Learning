05729d972d13611a9f42e8a9357a921de259e913
cryptogeekk <krishna_11822003@nitkkr.ac.in>
2021-04-16 21:30:49 +0530

    update

---
+++ dataset_divider.py
@@ -0,0 +1,36 @@
+from tensorflow import keras
+import pandas as pd
+import numpy as np
+
+def divide(parts, dataset_x, dataset_y):
+    fashion_mnist = keras.datasets.mnist
+    (X_train_full, y_train_full), (X_test, y_test) = fashion_mnist.load_data()
+    
+    #for temproary use
+    # X_train_full=np.arange(1,21)
+    # y_train_full=np.arange(21,41)
+    
+    
+    each_part_number=int(len(X_train_full)/parts)
+    list_x_train=[]
+    list_y_train=[]
+    
+    
+    # for x in range(0,parts):
+    #     list_x_train.append([])
+    #     list_y_train.append([])
+    
+    number_list=[]
+    for x in range(0,parts+1):
+        number_list.append(each_part_number*x)
+    
+    
+    for x in range(0,parts):
+        data_x=dataset_x[number_list[x]:number_list[x+1]]
+        data_y=y_train_full[number_list[x]:number_list[x+1]]
+        list_x_train.append(data_x)
+        list_y_train.append(data_y)
+        
+    return list_x_train, list_y_train
+
+    
